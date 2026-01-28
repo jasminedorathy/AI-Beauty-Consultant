cf06b6f05d975c84e1a52912d7643b977e040eb5
Aashif786 <jasminedorathyvasantharaj@gmail.com>
2026-01-28 11:14:18 +0530

    feat: Remove trust badges from landing page header

--- Frontend/frontend/src/pages/LandingPage.js
+++ Frontend/frontend/src/pages/LandingPage.js
@@ -43,28 +43,6 @@ const LandingPage = () => {
                             Sign In
                         </Link>
                     </div>
-
-                    {/* Trust Badges */}
-                    <div className="mt-12 flex flex-wrap justify-center gap-6 text-sm text-gray-600">
-                        <div className="flex items-center gap-2">
-                            <svg className="w-5 h-5 text-green-500" fill="currentColor" viewBox="0 0 20 20">
-                                <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
-                            </svg>
-                            97% Accuracy
-                        </div>
-                        <div className="flex items-center gap-2">
-                            <svg className="w-5 h-5 text-green-500" fill="currentColor" viewBox="0 0 20 20">
-                                <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
-                            </svg>
-                            Instant Results
-                        </div>
-                        <div className="flex items-center gap-2">
-                            <svg className="w-5 h-5 text-green-500" fill="currentColor" viewBox="0 0 20 20">
-                                <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
-                            </svg>
-                            100% Private
-                        </div>
-                    </div>
                 </div>
             </section>
 
